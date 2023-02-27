43e435875eaf1eee8d70fe7914bbb10b65f8b60d
hmongouachon <hadrien.mongouachon@gmail.com>
2023-02-27 18:36:43 +0100

    buld mobile marquees

--- wordpress/wp-content/themes/dixeed-2023/src/blocks/marquees/marquees-block.js
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/marquees/marquees-block.js
@@ -1,2 +1,5 @@
 // init marquees
 gsap.to('.marquee-inner', { xPercent: -50, ease: 'none', duration: 35, repeat: -1 });
+$(window).resize(function(){
+	gsap.to('.marquee-inner', { xPercent: -50, ease: 'none', duration: 30, repeat: -1 });
+})
