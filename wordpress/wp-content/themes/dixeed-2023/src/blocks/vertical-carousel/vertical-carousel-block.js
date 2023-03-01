3bfda8c86b8035e9ef95390dbccb02385f753866
hmongouachon <hadrien.mongouachon@gmail.com>
2023-03-01 18:59:09 +0100

    fix slick pauseOnHover bug p1

--- wordpress/wp-content/themes/dixeed-2023/src/blocks/vertical-carousel/vertical-carousel-block.js
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/vertical-carousel/vertical-carousel-block.js
@@ -1,42 +1,29 @@
 (function($) {
-  function initializeBlock($block) {
-     $block.slick({
-        autoplay: true,
-        arrows: false,
-        dots: false,
-        slidesToShow: 3,
-        // centerPadding: "10px",
-        // draggable: true,
-        infinite: true,
-        pauseOnHover: false,
-        // swipe: false,
-        // touchMove: false,
-        vertical: true,
-        verticalScrolling: true,
-        verticalSwiping : true,
-        speed: 5000,
-        autoplaySpeed: 5000,
-        useTransform: true,
-        cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1.000)',
-        adaptiveHeight: true,
-        // responsive: [{
-        //       breakpoint: 750,
-        //       settings: {
-        //          slidesToShow: 3,
-        //       }
-        //    },
-        //    {
-        //       breakpoint: 480,
-        //       settings: {
-        //          slidesToShow: 2,
-        //       }
-        //    }
-        // ]
-     });
+  // slick oauseOnHover does not work with infinite autoplay 
+  // todo : made custom gsap slider : https://codepen.io/akapowl/pen/RwjGVWq
+   function initializeBlock($block) {
+      $block.slick({
+         autoplay: true,
+         arrows: false,
+         dots: false,
+         // slidesToShow: 3,
+         draggable: false,
+         infinite: true,
+         pauseOnHover: false,
+         // swipe: false,
+         // touchMove: false,
+         vertical: true,
+         verticalSwiping: true,
+         speed: 5000,
+         autoplaySpeed: 0,
+         useTransform: true,
+         cssEase: 'linear',
+         adaptiveHeight: true,
+      });
    }
    if (window.acf) {
       // Initialize dynamic block preview (editor).
-      window.acf.addAction("render_block_preview", function ($el, attributes) {
+      window.acf.addAction("render_block_preview", function($el, attributes) {
          var $block = $el;
          if ($block.length) {
             initializeBlock($block);
@@ -44,11 +31,10 @@
       });
    } else {
       // Initialize each block on page load (front end).
-      $(document).ready(function () {
-         $(".vertical-carousel").each(function () {
+      $(document).ready(function() {
+         $(".vertical-carousel").each(function() {
             initializeBlock($(this));
          });
       });
    }
-   // https://codepen.io/LinhNN/pen/VWEoqW
 })(jQuery);
