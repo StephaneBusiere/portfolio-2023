924c85635170f617aca71b5d5734bdda366667b1
hmongouachon <hadrien.mongouachon@gmail.com>
2023-03-07 17:48:22 +0100

    add graphics slide 1

--- wordpress/wp-content/themes/dixeed-2023/src/blocks/horizontal-slider/horizontal-slider-block.js
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/horizontal-slider/horizontal-slider-block.js
@@ -9,28 +9,42 @@
          swipe: true,
          // touchMove: false,
          useTransform: true,
-         // cssEase: 'linear',
+         cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+         // fade: true,
+         speed: 900,
          adaptiveHeight: true,
          responsive: [{
-               breakpoint: 768,
-               settings: {
-                  slidesToShow: 3,
-                  vertical: false,
-                  verticalSwiping: false,
-                  adaptiveHeight: false,
-               }
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 3,
+               vertical: false,
+               verticalSwiping: false,
+               adaptiveHeight: false,
             }
-         ]
+         }]
       });
-         $(".cta-arrow-prev").click(function(e) {
-            e.preventDefault();
-            $block.slick("slickPrev");
-         });
+      $(".cta-arrow-prev").click(function(e) {
+         e.preventDefault();
+         $block.slick("slickPrev");
+      });
 
-         $(".cta-arrow-next").click(function(e) {
-            e.preventDefault();
-            $block.slick("slickNext");
-         });
+      $(".cta-arrow-next").click(function(e) {
+         e.preventDefault();
+         $block.slick("slickNext");
+      });
+
+      // todo : update title index + graphics animation
+      $block.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+          var CurrentSlideDom=$(slick.$slides.get(currentSlide));
+          var NextSlideDom=$(slick.$slides.get(nextSlide));
+      });
+      $block.on('afterChange', function(event, slick, currentSlide, nextSlide) {
+          var dataId = $('.slick-current').attr("data-slick-index");    
+          console.log(dataId);
+        //   console.log($('.pt-slick-carousel__slides .slick-active').attr('data-slick-index'));
+        // console.log($('.pt-slick-carousel__slides .slick-active').attr('id'));
+
+      });
    }
    // if (window.acf) {
    //    // Initialize dynamic block preview (editor).
@@ -43,11 +57,11 @@
    //       }
    //    });
    // } else {
-      // Initialize each block on page load (front end).
-      $(document).ready(function() {
-         $(".horizontal-slider").each(function() {
-            initializeBlock($(this));
-         });
+   // Initialize each block on page load (front end).
+   $(document).ready(function() {
+      $(".horizontal-slider").each(function() {
+         initializeBlock($(this));
       });
+   });
    // }
 })(jQuery);
