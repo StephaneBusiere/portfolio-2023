2b279ff6f8af9292b86cb1a4846ec628dbf481f0
Stéphane Busiere <7332724-BusiereStephane@users.noreply.gitlab.com>
2023-05-03 17:17:50 +0200

    fix various block template

--- wordpress/wp-content/themes/dixeed-2023/src/blocks/content-section/content-section-block.template.php
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/content-section/content-section-block.template.php
@@ -8,14 +8,26 @@
 
 $background_class = empty( $block['backgroundColor'] ) ? '' : ' has-' . $block['backgroundColor'] . '-background-color';
 $background_color = get_field( 'content-section-background-color' );
+$padding_choices  = get_field( 'section-padding' );
+
+
 ?>
 <section <?php ign_block_attrs( $block, 'section-content-block full-width' . $background_class ); ?>>
-	<div class="container-content" 
-	<?php
-	if ( $background_color ) :
+<?php
+if ( $padding_choices ) :
+	foreach ( $padding_choices as $padding_choice ) :
 		?>
-		style=background:<?php echo esc_attr( $background_color ); endif ?>>
-		<InnerBlocks />
+
+	<div class="container-content <?php echo $padding_choice; ?> " 
+		<?php
+	endforeach;
+endif;
+
+if ( $background_color ) :
+	?>
+	style=background:<?php echo esc_attr( $background_color ); endif ?>>
+	<InnerBlocks />
+
 	</div>
 
 </section>
