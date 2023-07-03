7f6642ff2e9ff19a91fa52a770a52dbe9073fc30
hmongouachon <hadrien.mongouachon@gmail.com>
2023-07-03 16:36:24 +0200

    add scrolltrigger part1

--- wordpress/wp-content/themes/dixeed-2023/src/blocks/team/team-block.js
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/team/team-block.js
@@ -1,6 +1,8 @@
-(function($) {
+
+
+(function ($) {
    function initializeBlock($block) {
-      
+
    }
    // if (window.acf) {
    //    // Initialize dynamic block preview (editor).
@@ -14,9 +16,47 @@
    //    });
    // } else {
    // Initialize each block on page load (front end).
-   $(document).ready(function() {
+   $(document).ready(function () {
       // $(".horizontal-slider").each(function() {
       //    initializeBlock($(this));
       // });
+
+      const tl = gsap.timeline({
+         scrollTrigger: {
+            trigger: '.team-block',
+            start: "top top",
+            end: window.innerHeight * 6,
+            pin: '.team-wrapper',
+            anticipatePin: 1,
+            // pinSpacing: false,
+            invalidateOnRefresh: true,
+            markers: true,
+            scrub: true
+         }
+      });
+      const items = document.querySelectorAll(".team-block li");
+      tl.add('sceneOne')
+         .call(function () {
+            // document.querySelectorAll(".shape-1")[0].style.zIndex = "1";
+         })
+         .to(items, {
+            y: - window.innerHeight,
+            stagger: .025,
+            // opacity: 1,
+            // css: {
+            //    width: 300,
+            //    height: 300
+            // },
+            // duration: 3
+         })
+         .to(items, {
+            opacity: 1,
+            // stagger: .025,
+            // css: {
+            //    width: 300,
+            //    height: 300
+            // },
+            // duration: 1
+         }, 0)
    });
 })(jQuery);
--- wordpress/wp-content/themes/dixeed-2023/src/blocks/team/team-block.template.php
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/team/team-block.template.php
@@ -10,18 +10,18 @@ $team_paragraph = get_field('team_paragraph');
 $team_people = get_field('team_people');
 ?>
 <div <?php ign_block_attrs($block, 'team-block full-width'); ?> data-scroll-section>
-	<svg class="ellipse-1" viewBox="0 0 1440 1476" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="720" cy="738" r="737.5" stroke="url(#paint0_linear_228_366)" />
-		<defs>
-			<linearGradient id="paint0_linear_228_366" x1="720" y1="0" x2="720" y2="1476"
-				gradientUnits="userSpaceOnUse">
-				<stop offset="0.0729167" stop-color="#F1F2EB" stop-opacity="0" />
-				<stop offset="0.479167" stop-color="#F2EFEB" />
-				<stop offset="0.895833" stop-color="#F2EFEB" stop-opacity="0" />
-			</linearGradient>
-		</defs>
-	</svg>
 	<div class="team-wrapper">
+		<svg class="ellipse-1" viewBox="0 0 1440 1476" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<circle cx="720" cy="738" r="737.5" stroke="url(#paint0_linear_228_366)" />
+			<defs>
+				<linearGradient id="paint0_linear_228_366" x1="720" y1="0" x2="720" y2="1476"
+					gradientUnits="userSpaceOnUse">
+					<stop offset="0.0729167" stop-color="#F1F2EB" stop-opacity="0" />
+					<stop offset="0.479167" stop-color="#F2EFEB" />
+					<stop offset="0.895833" stop-color="#F2EFEB" stop-opacity="0" />
+				</linearGradient>
+			</defs>
+		</svg>
 		<svg class="ellipse-2" viewBox="0 0 1032 1032" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<circle cx="516" cy="516" r="515.5" stroke="#F2EFEB" />
 		</svg>
