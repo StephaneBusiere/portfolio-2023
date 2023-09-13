501606af83c86dac3f28200b61cf9efe9b807cff
Stéphane Busiere <7332724-BusiereStephane@users.noreply.gitlab.com>
2023-09-13 10:03:45 +0200

    fix responsive issues

--- wordpress/wp-content/themes/dixeed-2023/src/blocks/cta/cta-block.template.php
+++ wordpress/wp-content/themes/dixeed-2023/src/blocks/cta/cta-block.template.php
@@ -11,7 +11,7 @@ $background_color = get_field( 'background_color_cta' );
 ?>
 <div <?php ign_block_attrs( $block, 'cta-block full-width' ); ?>  style="background-color:<?php echo esc_attr( $background_color ); ?>" data-scroll-section>
 	<div class="cta <?php echo $cta_align; ?>">
-		<a href="<?php echo esc_url( $cta_link['url'] ); ?>" title="<?php echo esc_attr( $cta_link['title'] ); ?>">
+		<a href="<?php echo esc_url( $cta_link['url'] ); ?>" target="<?php echo esc_url( $cta_link['target'] ); ?>" title="<?php echo esc_attr( $cta_link['title'] ); ?>">
 			<span class="shape"></span>
 			<span class="text"><?php echo esc_attr( $cta_link['title'] ); ?></span>
 		</a>
